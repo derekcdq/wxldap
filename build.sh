d01eca07b78672f2c6f35f2212e4f004a7a60f53
derek.chen <derek.chen@eclincloud.com>
2021-06-17 18:31:56 +0800

    修改编译脚本

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "开始检查需要的依赖包...."
 
-for i in `grep -E "github.com|golang.org" wxopld/*.go|awk '{print $2 }'|sed s/\"//g`
+for i in `grep -E "github.com|golang.org" model/*.go|awk '{print $2 }'|sed s/\"//g`
 do
         echo "开始安装 $i"
         go get $i
@@ -15,8 +15,8 @@ do
 done
 
 echo "开始编译Linux版本.."
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/wx2openldap_linux main.go && echo "二进制文件已经生成到了bin目录下"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/wxldap_linux main.go && echo "二进制文件已经生成到了bin目录下"
 echo "开始编译Windows版本.."
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/wx2openldap_win32 main.go && echo "二进制文件已经生成到了bin目录下"
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/wxldap_win32 main.go && echo "二进制文件已经生成到了bin目录下"
 echo "开始编译Mac版本.."
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o bin/wx2openldap_mac main.go && echo "二进制文件已经生成到了bin目录下"
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o bin/wxldap_mac main.go && echo "二进制文件已经生成到了bin目录下"
